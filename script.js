4633da2ea0b8cb2bede22bb9c740cdbae167c6e4
Bia-source <bia_ferreirad@yahoo.com>
2025-01-28 20:16:02 -0300

    mais exemplos de funcoes

--- script.js
+++ script.js
@@ -5,37 +5,50 @@ console.log("aula 16 modulo 1")
 
 // declarando uma variavel
 var nome = "Beatriz";
-
 // abrir e fechar colchetes significa inicio e fim de um
 // bloco de código {}
 
 // declarando uma função 
-function exibirNome(){
- // executado
- console.log(nome)
+function exibirNome() {
+    // executado
+    console.log(nome);
 }
+
+function exibirNome3() {
+    // executado
+    console.log(nome);
+}
+
+function exibirNome1() {
+
+}
+
+function exibirNome2() {
+
+}
+
 
 // chamando função 
 exibirNome();
 
 
 // declarando função
-function andar(){
+function andar() {
     console.log("andando");
 }
 
 // chamando a função
 andar();
 
-
 // declarando função
-function socar(){
+function socar() {
     console.log("socando o sub-zero")
 }
 
 // chamando função -> apertar botao bolinha
 socar();
 
+// LIVE CODING
 
 
 
