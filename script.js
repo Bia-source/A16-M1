69d687c0ec18388982dd4e28f59da29e0f78edf4
Bia-source <bia_ferreirad@yahoo.com>
2025-01-28 20:27:42 -0300

    live coding

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
-console.log("aula 16 modulo 1")
+// console.log("aula 16 modulo 1")
 
-// FUNÇÕES -> Algo a ser feito
-// FUNÇÕES -> O que deve ser executado/ acionado
+// // FUNÇÕES -> Algo a ser feito
+// // FUNÇÕES -> O que deve ser executado/ acionado
 
-// declarando uma variavel
-var nome = "Beatriz";
-// abrir e fechar colchetes significa inicio e fim de um
-// bloco de código {}
+// // declarando uma variavel
+// var nome = "Beatriz";
+// // abrir e fechar colchetes significa inicio e fim de um
+// // bloco de código {}
 
-// declarando uma função 
-function exibirNome() {
-    // executado
-    console.log(nome);
-}
+// // declarando uma função 
+// function exibirNome() {
+//     // executado
+//     console.log(nome);
+// }
 
-function exibirNome3() {
-    // executado
-    console.log(nome);
-}
+// function exibirNome3() {
+//     // executado
+//     console.log(nome);
+// }
 
-function exibirNome1() {
+// function exibirNome1() {
 
-}
+// }
 
-function exibirNome2() {
+// function exibirNome2() {
 
-}
-
-
-// chamando função 
-exibirNome();
+// }
 
 
-// declarando função
-function andar() {
-    console.log("andando");
-}
+// // chamando função 
+// exibirNome();
 
-// chamando a função
-andar();
 
-// declarando função
-function socar() {
-    console.log("socando o sub-zero")
-}
+// // declarando função
+// function andar() {
+//     console.log("andando");
+// }
 
-// chamando função -> apertar botao bolinha
-socar();
+// // chamando a função
+// andar();
 
+// // declarando função
+// function socar() {
+//     console.log("socando o sub-zero")
+// }
+
+// // chamando função -> apertar botao bolinha
+// socar();
+
+// como pensar em nome de função
+// verbos -> ação -> funÇÃO
 // LIVE CODING
 
+// crie uma função
+function cumprimentar(){
+    // utilizando a função prompt
+    // para pegando nome do usuario
 
+    // armazenando o nome que o usuario informou prompt("digite seu nome");
+    // na variavel var nomeUsuario =
+    var nomeUsuario = prompt("digite seu nome");
+    
+    // mostrando mensagem de Olá para o usuario
+    alert("Óla, " + nomeUsuario);
+}
+
+cumprimentar();
 
 
 
