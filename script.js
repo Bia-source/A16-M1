269513e0a84eb1350536281fad8a2aa9bbe02c15
Bia-source <bia_ferreirad@yahoo.com>
2025-01-28 20:06:26 -0300

    declarando primeira função

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 console.log("aula 16 modulo 1")
+
+// FUNÇÕES -> Algo a ser feito
+// FUNÇÕES -> O que deve ser executado/ acionado
+
+// declarando uma variavel
+var nome = "Beatriz";
+
+// abrir e fechar colchetes significa inicio e fim de um
+// bloco de código {}
+
+// declarando uma função 
+function exibirNome(){
+ // executado
+ console.log(nome)
+}
+
+// chamando função 
+exibirNome();
+
+
+// declarando função
+function andar(){
+    console.log("andando");
+}
+
+// chamando a função
+andar();
+
+
+// declarando função
+function socar(){
+    console.log("socando o sub-zero")
+}
+
+// chamando função -> apertar botao bolinha
+socar();
+
+
+
+
+
+
+
+
+
+
+
+
